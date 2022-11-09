5f9a06de5b993c7a0ccf01de47070f80c4f3c65f
Igor Santiago Macedo <igor.santiago546@outlook.com>
2022-11-09 01:37:45 -0300

    Data

--- aula4.php
+++ aula4.php
@@ -10,8 +10,14 @@ if(isset($_POST['calcular']))
     $taxa    = $_POST['taxa'];
     $parcela = $_POST['parcela'];
     $coeficiente = parcelar(floatval($taxa), intval($parcela));
-
-    $parcelas = ([$capital, $taxa,$parcela,$coeficiente*$capital]);
+print_r($_POST);
+    $data = date('d/m/Y');
+    $dias = 30;
+    for ($i=0; $i < $parcela; $i++) { 
+        $parcelas += ([($i+1).'ª',($coeficiente*floatval($capital)),date($data,strtotime('+'.$dias.' days'))]);
+        $dias += 30; 
+    }
+    print_r($parcelas);
 }
 
 
@@ -36,7 +42,15 @@ if(isset($_POST['calcular']))
     </form>
     <br><hr>
     <?php if(count($parcelas)>0){?> 
-    <h3>Valor da parcela:R$ <?php echo $parcelas[3]; ?></h3>
-    <?php } ?>
+    <h3>Valor da capital:R$ <?php echo $capital; ?></h3>
+    <h3>taxa de juro: <?php echo $taxa; ?> %</h3>
+    <h3>Parcelas: <?php echo $parcela; ?> meses</h3>
+    <?php 
+        foreach ($parcelas as $valores) {
+            ?>
+                <h4><?php echo($valores[0]." R$ ". strval($valores[1])." - ".strval($valores[2]));?></h4>
+            <?php 
+        }
+    } ?>
 </body>
 </html>
