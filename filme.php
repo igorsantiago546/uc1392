38085d1411b453fe9e1167752377dcc17df0043d
Igor Santiago Macedo <igor.santiago546@outlook.com>
2022-11-23 00:36:03 -0300

    Filme Incompleto

--- filme.php
+++ filme.php
@@ -3,14 +3,17 @@
 include 'conecta.php';
 
 // cria a consulta sql
-$consulta = "select * from filme";
+$consulta = "SELECT * FROM filme where deleted is null order by titulo, cod_filme asc";
+$consultaSqlArq = "SELECT * FROM filme where deleted is not null order by titulo, cod_filme asc";
 
 // trazer a lista completa dos dados 
 $lista = $pdo->query("$consulta");
+$listaArq = $pdo->query($consultaSqlArq);
 
 // separar os dados em linhas
 
 $linha = $lista->fetch();
+$linhaArq = $listaArq->fetch();
 $num_linhas = $lista->rowCount();
 // echo 'A consulta retornou <stong>'.$num_linhas. ' </strong> Filmes <br>';
 
@@ -20,27 +23,85 @@ $num_linhas = $lista->rowCount();
 
 //print_r($linha);
 
+// busca filme por codigo
+$titulo = "";
+$sinopse = "";
+$lancamento = "";
+$pais_origem = "";
+$duracao = "";
+$preco = "";
+$cod = 0;
+if (isset($_GET['codedit']))
+{
+    $queryEdit = "SELECT * FROM filme where cod_filme=".$_GET['codedit'];
+    $filme = $pdo->query($queryEdit)->fetch();
+    $cod = $_GET['codedit'];
+    $titulo = $_GET['titulo'];
+    $sinopse = $_GET['sinopse'];
+    $lancamento = $_GET['lancamento'];
+    $pais_origem = $_GET['pais_origem'];
+    $duracao = $_GET['duracao'];
+    $preco = $_GET['preco'];
+    
+}
+
+// codigo para arquivar(excluir)
+if (isset($_GET['codarq']))
+{
+    $queryArq = "update filme set deleted = now() where cod_filme=".$_GET['codarq'];
+    $filme = $pdo->query($queryArq)->fetch();
+    header('location: filme.php');
+}
+
+// restaurar filme
+if (isset($_GET['codres']))
+{
+    $queryArq = "update filme set deleted = null where cod_filme=".$_GET['codres'];
+    $filme = $pdo->query($queryArq)->fetch();
+    header('location: filme.php');
+}
+
+// remover definitivamente (LGPD)
+if (isset($_GET['codexc']))
+{
+   $queryExc = "delete from filme where cod_filme=".$_GET['codexc'];
+    $filme = $pdo->query($queryExc)->fetch();
+    header('location: filme.php');
+}
+
 if(isset($_POST['enviar'])) // inserir ou alterar
 {
-    $titulo  = $_POST['titulo'];
+    $titulo = $_POST['titulo'];
     $sinopse = $_POST['sinopse'];
     $lancamento = $_POST['lancamento'];    
     $pais_origem = $_POST['pais_origem'];   
     $duracao = $_POST['duracao'];   
     $preco = $_POST['preco'];
-    $genero = $_POST['genero'];
-    $elenco = $_POST['elenco'];
-    $direcao = $_POST['direcao'];
-    $idioma = $_POST['idioma'];   
-    $legenda = $_POST['legenda'];
+    // $genero = $_POST['genero'];
+    // $elenco = $_POST['elenco'];
+    // $direcao = $_POST['direcao'];
+    // $idioma = $_POST['idioma'];   
+    // $legenda = $_POST['legenda'];
     
-    $consulta = "insert filme (titulo, sinopse, lancamento, pais_origem, duracao, preco, genero, elenco, direcao, idioma, legenda) values ('$titulo','$sinopse','$lancamento','$pais_origem','$duracao','$preco','$genero','$elenco','$direcao','$idioma','$legenda')";
+    $consulta = "insert filme (titulo, sinopse, lancamento, pais_origem, duracao, preco) values ('$titulo','$sinopse','$lancamento','$pais_origem','$duracao','$preco')";
     $resultado = $pdo->query($consulta);
     $_POST['enviar'] = null ;
 
     header('location: filme.php');
 }
-
+if (isset($_POST['alterar']))
+{
+    $cod = $_POST['cod-filme'];
+    $titulo = $_POST['titulo'];
+    $sinopse = $_POST['sinopse'];
+    $lancamento = $_POST['lancamento'];
+    $pais_origem = $_POST['pais_origem'];
+    $duracao = $_POST['duracao'];
+    $preco = $_POST['preco'];
+    $updateSql = "update filme set titulo = '$titulo', sinopse='$sinopse', lancamento='$lancamento', pais_origem='$pais_origem', duracao='$duracao', preco='$preco' where cod_filme = $cod";
+    $resultado = $pdo->query($updateSql);
+    header('location: filme.php');
+}
 
 
 ?>
@@ -51,7 +112,7 @@ if(isset($_POST['enviar'])) // inserir ou alterar
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/style.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Filmes <?php echo '(' .$num_linhas.')' ?></title>
+    <title>Filmes (<?php echo $num_linhas; ?>)</title>
     <!-- <style>
     /* td{
         border-bottom: 1px solid black; 
@@ -63,46 +124,46 @@ if(isset($_POST['enviar'])) // inserir ou alterar
             <div hidden>
                 <label for="cod-filme">
                     Código
-                    <input type="text" name="cod-filme">
+                    <input type="text" name="cod-filme" value="<?php echo $cod;?>">
                 </label>
             </div>
             <div>
             <label for="titulo">
                     Título
-                    <input type="text" name="titulo" required>
+                    <input type="text" name="titulo" required value="<?php echo $titulo;?>">
                 </label>
             </div>
             <div>
             <label for="sinopse">
                     Sinopse
-                    <input type="textarea" name="sinopse" required>
+                    <input type="textarea" name="sinopse" required value="<?php echo $sinopse;?>">
                 </label>
             </div>
             <div>
             <label for="lancamento">
                     Lançamento
-                    <input type="number" name="lancamento" required>
+                    <input type="date" name="lancamento" required value="<?php echo $lancamento;?>">
                 </label>
             </div>
             <div>
             <label for="pais_origem">
                     Pais de Origem
-                    <input type="text" name="pais_origem" required>
+                    <input type="text" name="pais_origem" required value="<?php echo $pais_origem;?>">
                 </label>
             </div>
             <div>
             <label for="duracao">
                     Duração
-                    <input type="number" name="duracao" required>
+                    <input type="number" name="duracao" required value="<?php echo $duracao;?>">
                 </label>
             </div>
             <div>
             <label for="preco">
                     Preço
-                    <input type="number" name="preco" required>
+                    <input type="number" name="preco" required value="<?php echo $preco;?>">
                 </label>
             </div>
-            <div>
+            <!-- <div>
             <label for="genero">
                     Genero
                     <input type="text" name="genero" required>
@@ -137,29 +198,68 @@ if(isset($_POST['enviar'])) // inserir ou alterar
                     Cod_classificacao
                     <input type="number" name="cod_classificacao" required>
                 </label>
-            </div>
+            </div> -->
             <div>
-                <button type="submit" name="enviar">Enviar</button>
+            <button type="submit" name="<?php echo $cod<1?'enviar':'alterar'; ?>"><?php echo $cod<1?'Enviar':'Alterar'; ?></button>
             </div>
         </form>
     </section>
+    <h3>Filmes Ativos</h3>
      <table>
         <thead>
-            <th>ID</th>
+            <th hidden>ID</th>
             <th>Título</th>
             <th>Sinopse</th>
             <th>Lançamento</th>
+            <th>Pais de Origem</th>
+            <th>Duração</th>
+            <th>Preço</th>
+            <th colspan="2">Ações</th>
         </thead>
         <tbody>
             <?php do {?>
                 <tr>
-                    <td><?php echo $linha['cod_filme'];?></td>
+                    <td hidden><?php echo $linha['cod_filme'];?></td>
                     <td><?php echo $linha['titulo'];?></td>
                     <td><?php echo $linha['sinopse'];?></td>
                     <td><?php echo $linha['lancamento'];?></td>
+                    <td><?php echo $linha['pais_origem'];?></td>
+                    <td><?php echo $linha['duracao'];?></td>
+                    <td><?php echo $linha['preco'];?></td>
+                    <td><a href="filme.php?codedit=<?php echo $linha['cod_filme'];?>">Editar</a></td>
+                    <td><a href="filme.php?codarq=<?php echo $linha['cod_filme'];?>">Arquivar</a></td>
                 </tr>
-            <?php }while($linha = $lista->fetch());?>
+            <?php } while ($linha = $lista->fetch())?>
         </tbody>
     </table> 
+    <h3>Filmes Arquivados</h3>
+    <table>
+        <thead>
+        <th hidden>ID</th>
+            <th>Título</th>
+            <th>Sinopse</th>
+            <th>Lançamento</th>
+            <th>Pais de Origem</th>
+            <th>Duração</th>
+            <th>Preço</th>
+            <th colspan="2">Ações</th>
+        </thead>
+        <tbody>
+            <?php do {?>
+                <tr>
+                <td hidden><?php echo $linhaArq['cod_filme'];?></td>
+                    <td><?php echo $linhaArq['titulo'];?></td>
+                    <td><?php echo $linhaArq['sinopse'];?></td>
+                    <td><?php echo $linhaArq['lancamento'];?></td>
+                    <td><?php echo $linhaArq['pais_origem'];?></td>
+                    <td><?php echo $linhaArq['duracao'];?></td>
+                    <td><?php echo $linhaArq['preco'];?></td>
+                    <td><a href="filme.php?codres=<?php echo $linhaArq['cod_filme'];?>">Restaurar</a></td>
+                    <td><a href="filme.php?codexc=<?php echo $linhaArq['cod_filme'];?>">Excluir</a></td>
+                
+                </tr>
+            <?php } while ($linhaArq = $listaArq->fetch())?>
+        </tbody>
+    </table>
 </body>
 </html>
